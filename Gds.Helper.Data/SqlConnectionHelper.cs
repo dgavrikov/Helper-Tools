d51ee761b13801fe48c05bd8070254b0cb8625a2
Dmitriy Gavrikov <dgavrikov84@gmail.com>
2019-08-20 23:35:26 +0300

    Add DataExtention and undefined code.

---
+++ Gds.Helper.Data/SqlConnectionHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Gds.Helper.Data
+{
+    /// <summary>
+    /// Provide helper method for SqlConnection.
+    /// </summary>
+    public static class SqlConnectionHelper
+    {
+        /// <summary>
+        /// Open SqlConnection and execute optimal SET'S for queries.
+        /// </summary>
+        /// <param name="connection">SqlConnection</param>
+        /// <param name="logException">Log delegate.</param>
+        /// <returns>Open or not.</returns>
+        public static bool ReConnect(this SqlConnection connection, Action<SqlException> logException = null)
+        {
+            if (connection.State == ConnectionState.Open) return true;
+            try
+            {
+                connection.Open();
+                using (var sql = connection.CreateCommand())
+                {
+                    sql.CommandText = @"SET ANSI_WARNINGS ON";
+                    try
+                    {
+                        sql.ExecuteNonQuery();
+                    }
+                    catch (SqlException) { }
+
+                    sql.CommandText = @"SET ARITHABORT ON";
+                    try
+                    {
+                        sql.ExecuteNonQuery();
+                    }
+                    catch (SqlException) { }
+                }
+                return true;
+            }
+            catch (SqlException sqlException)
+            {
+                logException?.Invoke(sqlException);
+                return false;
+            }
+        }
+    }
+}
---
+++ undefined code/MultiThreadingHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Gds.Helper.WinForms
+{
+    /// <summary>
+    /// Provide method for multi threading helper.
+    /// </summary>
+    public static class MultiThreadingHelper
+    {
+            /// <summary>
+            /// Check need invoke for execute delegate.
+            /// </summary>
+            /// <param name="control">Control</param>
+            /// <param name="action">Delegate</param>
+            public static void InvokeEx(this Control control, Action action)
+            {
+                if (control.InvokeRequired)
+                    control.Invoke(action);
+                else
+                    action();
+            }
+        /// <summary>
+        /// Check need invoke for execute delegate.
+        /// </summary>
+        /// <typeparam name="T">Type of parameter delegate.</typeparam>
+        /// <param name="control">Control.</param>
+        /// <param name="action">Delegate.</param>
+        /// <param name="obj">Parameter for delegate.</param>
+        public static void InvokeEx<T>(this Control control, Action<T> action, T obj)
+            {
+                if (control.InvokeRequired)
+                    control.Invoke(action, obj);
+                else
+                    action(obj);
+            }
+    }
+}
